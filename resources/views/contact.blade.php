33af435865cd0635aec453f1889982063de44cba
mbattou <mbatt065@uottawa.ca>
2017-02-02 13:35:34 -0500

    Added: info alert to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,12 @@
 @section('content')
 
 <h1>Contact</h1>
-<P>Please, feel free to contact {{ $data['admin'] }} at: {{ $data['email'] }}</P>
+<br/><br/>
+<!-- SUCCESS ALERT -->
+<div class="col-md-6">
+<div class="alert alert-info">
+  <strong>Info!</strong> Please, feel free to contact {{ $data['admin'] }} at:<strong> {{ $data['email'] }}</strong>.
+</div>
+</div>
 
 @endsection
